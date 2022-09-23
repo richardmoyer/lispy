4547b916c9de464e92d126150d6ca3fa95a7f4d3
richmoyer <richmoyer@protonmail.com>
2022-09-23 10:08:15 -0500

    comment added

--- prompt.c
+++ prompt.c
@@ -6,37 +6,37 @@
 
 static char buffer[2048];
 
-char* readline(char* prompt) {
-    fputs(prompt, stdout);
-    fgets(buffer, 2048, stdin);
-    char* cpy = malloc(strlen(buffer)+1);
-    strcpy(cpy, buffer);
-    cpy[strlen(cpy)-1] = '\0';
-    return cpy;
+char *readline(char *prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char *cpy = malloc(strlen(buffer) + 1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy) - 1] = '\0';
+  return cpy;
 }
 
-void add_history(char* unused) {}
+void add_history(char *unused) {}
 
 #else
-#include <editline/readline.h>
 #include <editline/history.h>
+#include <editline/readline.h>
 #endif
 
+int main(int argc, char **argv) {
 
-int main(int argc, char** argv) {
+  puts("LISPY Version 0.0.0.0.1");
+  puts("Press Ctrl+C to Exit\n");
 
-    puts("LISPY Version 0.0.0.0.1");
-    puts("Press Ctrl+C to Exit\n");
+  while (1) {
 
-    while (1) {
+    char *input = readline("lispy-> ");
+    add_history(input);
 
-        char* input = readline("lispy-> ");
-        add_history(input);
+    printf("No, you're a %s\n", input);
+    free(input);
+  }
 
-        printf("No, you're a %s\n", input);
-        free(input);
-
-    }
-
-    return 0;
+  return 0;
 }
+
+/* comment here */
